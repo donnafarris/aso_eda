13483a16b992dd91d8f82b7cfe6843b7293d1050
donnafarris <dlfarris97@gmail.com>
2024-07-22 14:09:50 -0600

    Corrected

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 from joblib import load
 import streamlit as st
+import os
 from local import ARTIFACTS_PATH
 from sat_growth_over_time import get_sat_growth_over_time_plot, get_starlink_vs_all_other_sats_plot
 from annual_launches_by_country import get_annual_launches_by_org_plot
@@ -8,77 +9,41 @@ from annual_launches_by_sat_type import get_launch_count_by_sat_class_plot
 
 @st.cache_data
 def load_data(filepath):
-    """
-    Loads data from a specified file using Joblib.
-
-    Parameters:
-    filepath (str or Path): The path to the file containing the data to be loaded.
-
-    Returns:
-    object: The data loaded from the file, typically a Python object serialized with Joblib.
-
-    The function opens the specified file in binary read mode, loads the data using Joblib, and returns the loaded data.
-    """
     with open(filepath, 'rb') as f:
         return load(f)
 
 
-# Dictionary to map filenames to functions
-PLOTS = {
-    "Satellite Growth Over Time": {
-        "filename": "launch_decay_orbit_over_time",
-        "function": get_sat_growth_over_time_plot
-    },
-    "Starlink vs All Other Satellites": {
-        "filename": "starlink_vs_other_launches",
-        "function": get_starlink_vs_all_other_sats_plot
-    },
-    "Annual Number of Launches by Country": {
-        "filename": "annual_launches_by_country",
-        "function": get_annual_launches_by_org_plot
-    },
-    "Annual Number of Launches by Satellite Type": {
-        "filename": "launch_count_by_sat_class",
-        "function": get_launch_count_by_sat_class_plot
-    }
+filenames = {
+    'launch_decay_orbit_over_time': 'Satellite Growth Over Time',
+    'starlink_vs_other_launches': 'Starlink vs All Other Satellites',
+    'annual_launches_by_country': 'Annual Number of Launches by Country',
+    'launch_count_by_sat_class': 'Annual Number of Launches by Satellite Type'
 }
 
+data = {key: load_data(os.path.join(ARTIFACTS_PATH, f"{key}.joblib"))
+        for key in filenames.keys()}
 
-def render_plot(choice, data):
-    """
-    Renders the selected plot in the Streamlit app.
-
-    Parameters:
-    choice (str): The title of the plot to render, corresponding to a key in the PLOTS dictionary.
-    data (dict): A dictionary containing the loaded data files, where keys are filenames and values are the data.
-
-    Returns:
-    None: The function directly renders the plot using Streamlit.
-
-    The function retrieves the appropriate plot function and data based on the user's choice,
-    generates the plot, and then displays it in the Streamlit app.
-    """
-    plot_info = PLOTS[choice]
-    fig = plot_info["function"](display_data=data[plot_info["filename"]])
-    st.plotly_chart(fig)
-
+plot_functions = {
+    "Satellite Growth Over Time": get_sat_growth_over_time_plot,
+    "Starlink vs All Other Satellites": get_starlink_vs_all_other_sats_plot,
+    "Annual Number of Launches by Country": get_annual_launches_by_org_plot,
+    "Annual Number of Launches by Satellite Type": get_launch_count_by_sat_class_plot
+}
 
 if __name__ == "__main__":
-    # Load all data files into a dictionary
-    data = {name: load_data(
-        ARTIFACTS_PATH / f"{info['filename']}.joblib") for name, info in PLOTS.items()}
-
     st.title("Visualizations Dashboard")
 
     # Sidebar for navigation
     st.sidebar.title("Navigation")
-    options = ["Home"] + list(PLOTS.keys())
+    options = ["Home"] + list(plot_functions.keys())
     choice = st.sidebar.selectbox("Select a page", options)
 
     if choice == "Home":
         st.write("Welcome to the Visualizations Dashboard!")
-    if choice == "Home":
-        st.write("Welcome to the Visualizations Dashboard!")
     else:
         st.header(choice)
-        render_plot(choice, data)
+        plot_func = plot_functions[choice]
+        filename = list(filenames.keys())[
+            list(filenames.values()).index(choice)]
+        fig = plot_func(display_data=data[filename])
+        st.plotly_chart(fig)
